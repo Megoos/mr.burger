e02d7e6a05cdfd47d68ab0a4709097597ba609cb
Megoos <meguskov@gmail.com>
2017-08-13 00:30:01 +0500

    add device menu js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  
+  $('.hamburger-menu-link').on('click', function(e){
+   e.preventDefault()
+    $('.device-menu').fadeIn();
+  })
+
+  $('.close').on('click', function(e){
+   e.preventDefault()
+    $('.device-menu').fadeOut();
+  })
+ 
+});
