7f997ee4c5c8f44d1d52f561a61c1b7e2bcbc086
Megoos <meguskov@gmail.com>
2017-08-22 01:20:04 +0500

    made one page scrolling :hamburger:

--- js/pageScroll.js
+++ js/pageScroll.js
@@ -3,6 +3,10 @@ $(function(){
         display = $('.maincontent'),
         inScroll = false;
 
+        var md = new MobileDetect(window.navigator.userAgent),
+            isMobile = md.mobile();
+
+    //переход на нужную секцию
     var performTransition = function (sectionEq) {
 
         if (inScroll) return
@@ -27,25 +31,75 @@ $(function(){
 
     }
 
-     $('.wrapper').on('wheel', function (e) {
-        var deltaY = e.originalEvent.deltaY;
+    // поиск секций
+    var defineSections = function (sections) {
         var activeSection = sections.filter('.active');
-        var nextSection = activeSection.next();
-        var prevSection = activeSection.prev();
-        
-        if(deltaY > 0  && nextSection.length) {
-            console.log('вверх');
-            performTransition(nextSection.index());
+        return {
+        activeSection: activeSection,
+        nextSection: activeSection.next(),
+        prevSection: activeSection.prev()
         }
-        
-        if(deltaY < 0 && prevSection.length) {
-            console.log('вниз');
-            performTransition(prevSection.index());
+    }
+
+    //общая функция для нескольких действий
+    var scrollToSection = function (direction) {
+        var section = defineSections(sections);
+
+        if (direction == 'up' && section.nextSection.length) { // скроллим вверх
+        performTransition(section.nextSection.index());
         }
-        
+
+        if (direction == 'down' && section.prevSection.length) { //скроллим вниз
+        performTransition(section.prevSection.index());
+        }
+    }
+
+    //действие на движение скрола
+     $('.wrapper').on({
+        wheel: function(e) {
+            var deltaY = e.originalEvent.deltaY;
+            var direction = deltaY > 0
+                ? 'up'
+                : 'down';
+
+            scrollToSection(direction)
+            
+        },
+        touchmove: function (e) {
+            e.preventDefault();
+        }
      });
 
+    //действие на кнопки
+     $(document).on('keydown', function (e) {
+        var section = defineSections(sections);        
+
+        switch (e.keyCode) {
+        case 40: //вверх
+            scrollToSection('up');
+            break;
+        case 38: //вниз
+            scrollToSection('down');
+            break;
+        }
+    });
+
+    $('[data-scroll-to]').on('click touchstart', function (e) {
+        e.preventDefault();
+
+        var elem = $(e.target);
+        var sectionNum = parseInt(elem.attr('data-scroll-to'));
+        performTransition(sectionNum);
+    });
     
+    //свайпы ТОЛЬКО для мобильных уст-в
+    if (isMobile) {
+        $(window).swipe({
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            scrollToSection(direction);
+        }
+        });
+    }
 
 
 //   $('.maincontent').fullpage({
